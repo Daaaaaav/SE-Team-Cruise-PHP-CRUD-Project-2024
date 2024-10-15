71dd370b8e09ff356b01aaa98409fe13da2ee754
nava-win <144338151+nava-win@users.noreply.github.com>
2024-10-15 15:09:54 +0700

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,12 @@
+<?php
+require_once("header.php");
+?>
+
+<nav class="ul">
+   <a class="li" href="index.php">Ferries</a>
+   <a class="li" href="listOwnership.php">Company</a>
+   <a class="li" href="listCountry.php">Country</a>
+   <a class="li" href="listRoom.php">Room</a>
+   <a class="li" href="listPassenger.php">Passenger</a>
+   <a class="li" href="listCrew.php">Crew</a>
+</nav>
